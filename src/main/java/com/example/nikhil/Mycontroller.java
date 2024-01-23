bfdfa965b41c53733442c1e14dbd0890010fdd3b
Harikesh Mohan Yadav <92807366+harrysince1999@users.noreply.github.com>
2024-01-23 19:13:34 +0530

    first commit

---
+++ src/main/java/com/example/nikhil/Mycontroller.java
@@ -0,0 +1,13 @@
+package com.example.nikhil;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Mycontroller {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello, world!";
+    }
+}
---
+++ src/main/java/com/example/nikhil/NikhilApplication.java
@@ -0,0 +1,13 @@
+package com.example.nikhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikhilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikhilApplication.class, args);
+	}
+
+}
